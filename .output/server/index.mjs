1d10ecb3825d5849e34415de5bbc2b6b66c59de7
Tracy Franck Mukendi <tracymukendi017@gmail.com>
2026-06-07 11:16:31 +0100

    npm fix

--- .output/server/index.mjs
+++ .output/server/index.mjs
@@ -88,30 +88,30 @@ const assets = {
   "/assets/index-CIGW-MKW.css": {
     "type": "text/css; charset=utf-8",
     "etag": '"3cf7-GSfYBurqLbmcVM5PnUOstAgC2rk"',
-    "mtime": "2026-06-07T08:47:21.154Z",
+    "mtime": "2026-06-07T10:12:21.567Z",
     "size": 15607,
     "path": "../public/assets/index-CIGW-MKW.css"
+  },
+  "/assets/styles-B-Etngqb.css": {
+    "type": "text/css; charset=utf-8",
+    "etag": '"1a8d1-M5h6TBuMwGr08QokNBgCg6gdA3Y"',
+    "mtime": "2026-06-07T10:12:21.567Z",
+    "size": 108753,
+    "path": "../public/assets/styles-B-Etngqb.css"
   },
   "/assets/stadium-hero-LHPVA9f_.jpg": {
     "type": "image/jpeg",
     "etag": '"36338-6Dm9vFvrhmJm4uNqly4Fx2RvCII"',
-    "mtime": "2026-06-07T08:47:21.154Z",
+    "mtime": "2026-06-07T10:12:21.565Z",
     "size": 222008,
     "path": "../public/assets/stadium-hero-LHPVA9f_.jpg"
   },
-  "/assets/styles-DUlMHffY.css": {
-    "type": "text/css; charset=utf-8",
-    "etag": '"1a7be-dELRBLPXYf53yQkN8nPf2tkajeI"',
-    "mtime": "2026-06-07T08:47:21.154Z",
-    "size": 108478,
-    "path": "../public/assets/styles-DUlMHffY.css"
-  },
-  "/assets/index-SOurvA5B.js": {
+  "/assets/index-BvCEj4MP.js": {
     "type": "text/javascript; charset=utf-8",
-    "etag": '"aa3a8-a6RCBUaZfaJaMqs3AF5oPRp3pgU"',
-    "mtime": "2026-06-07T08:47:21.155Z",
-    "size": 697256,
-    "path": "../public/assets/index-SOurvA5B.js"
+    "etag": '"aa5d3-iDoXoOwdtdWBZ6qVpyaPIjgxP+g"',
+    "mtime": "2026-06-07T10:12:21.566Z",
+    "size": 697811,
+    "path": "../public/assets/index-BvCEj4MP.js"
   }
 };
 function readAsset(id) {
